39185901c93e58ee273a6beede4517daae17a82c
MelvinMbae <melvinmuthomi95@gmail.com>
2023-11-03 13:11:20 +0300

    models created and added to db

--- models.py
+++ models.py
@@ -13,20 +13,27 @@ Base = declarative_base()
 class Driver(Base):
     __tablename__ = 'drivers'
     __table_args__ = (
-        UniqueConstraint('email', name='unique_email')
+        UniqueConstraint('email', name='unique_email'),
     )
 
     id = Column(Integer, primary_key=True)
     name=Column(String)
+    email = Column(String(50))
     gender=Column(String)
     date_of_birth=Column(Date)
+    age=Column(Integer)
     years_of_experience=Column(Integer)
+    reviews = relationship('Driver_Review', backref=backref('driver'))
     
+    def __repr__(self):
+        return f"Driver(id={self.id}, " + \
+            f"name={self.name}, " + \
+            f"years_of_experience = {self.years_of_experience})"
+        
     
-
 class Car(Base):
     __tablename__ = 'cars'
-    __table_args__ = (UniqueConstraint('licence_plate',name='unique_licence_plate'))
+    __table_args__ = (UniqueConstraint('licence_plate',name='unique_licence_plate'),)
     
     id = Column(Integer, primary_key=True)
     make = Column(String)
@@ -42,3 +49,25 @@ class Car(Base):
     availability = Column(Boolean)
     driver_id=Column(Integer, ForeignKey('drivers.id'))
     driver = relationship('Driver', backref=backref('car', uselist=False))
+    
+    def __repr__(self):
+        return f"Car(id={self.id}, " + \
+            f"make={self.make}, " + \
+            f"model={self.model}, " + \
+            f"licence_plate = {self.licence_plate}, " + \
+            f"driver_id = {self.driver_id})"
+                                        
+
+class Driver_Review(Base):
+    __tablename__ = 'driver_reviews'
+    
+    id = Column(Integer, primary_key=True)
+    rating = Column(Integer)
+    comment=Column(String)
+    
+    driver_id = Column(Integer, ForeignKey('drivers.id'))
+    
+    def __repr__(self):
+        return f'Driver_Review(id={self.id}, ' + \
+            f'rating={self.rating}, ' + \
+            f'driver_id={self.driver_id})'
