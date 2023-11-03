c5c5083eeb901a29c5a8921b052d186b48c8d66c
MelvinMbae <melvinmuthomi95@gmail.com>
2023-11-03 18:33:54 +0300

    install click

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 from sqlalchemy import create_engine, func
 
-from sqlalchemy.ext.declarative import declarative_base
-
 engine = create_engine('sqlite:///transport_hub.db')
 
+# Initialize a session
 Session = sessionmaker(bind=engine)
 session=Session()
-
-Base = declarative_base()
 
 #CAR METHODS
 
@@ -100,5 +97,4 @@ def remove_driver(driver_id):
         print("No such driver exists.")
     
 remove_driver(50)
-
 
